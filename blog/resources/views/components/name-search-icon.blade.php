7cbb870a1ce48285e436d242472c060b0bed1d94
thelaravelgod <113533326+thelaravelgod@users.noreply.github.com>
2022-09-17 16:28:55 +0100

    Name/Search layer
    responsive design for name/search cart layer

--- blog/resources/views/components/name-search-icon.blade.php
+++ blog/resources/views/components/name-search-icon.blade.php
@@ -9,6 +9,16 @@
                 <i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
+        <div class="name-search-trending">
+            <div style="font-weight: bold;">trending:</div>
+            <div>electronics</div>
+            <div>fast foods</div>
+            <div>gadgets</div>
+            <div>games</div>
+            <div>tech</div>
+            <div>fashion</div>
+            <div>movies</div>
+        </div>
     </div>
     <div class="name-search-end-wraps">
         <div class="name-search-cart-count">0</div>
